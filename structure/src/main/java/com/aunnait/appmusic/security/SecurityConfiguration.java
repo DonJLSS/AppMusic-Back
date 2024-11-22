6f33e56ce00154466eab62c435951d92c5fc2593
DonJLSS <serrsalcjl@gmail.com>
2024-11-22 11:48:55 +0100

    Configuración CORS básica y modificado el banner

--- structure/src/main/java/com/aunnait/appmusic/security/SecurityConfiguration.java
+++ structure/src/main/java/com/aunnait/appmusic/security/SecurityConfiguration.java
@@ -3,9 +3,15 @@ package com.aunnait.appmusic.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,14 +23,29 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { //Applies before reaching controllers
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers(HttpMethod.POST).authenticated()
+                        .requestMatchers(HttpMethod.GET).hasRole("admin")
+                        .anyRequest().authenticated()
                 )
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults());
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() { //CORS basic configuration
+        CorsConfiguration cc = new CorsConfiguration();
+        cc.setAllowedOrigins(Arrays.asList("/*"));
+        cc.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        cc.setAllowedHeaders(Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type", "Authorization"));
+        cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        cc.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", cc);
+        return source;
     }
 }
